9dd4e5799b8c7f3df3816879ad1c8829788bf67e
Lucy18 <916648359@qq.com>
2017-04-25 00:54:33 +0800

    535

--- workspace/Leetcode/src/leetcode/Encode_and_Decode_TinyURL_535.java
+++ workspace/Leetcode/src/leetcode/Encode_and_Decode_TinyURL_535.java
@@ -1,7 +1,23 @@
 package leetcode;
 
-public class Encode_and_Decode_TinyURL_535 {
+import java.util.ArrayList;
 
+public class Encode_and_Decode_TinyURL_535 {
+	ArrayList<String> urLs=new ArrayList<String>();
+
+	 // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+        int index=urLs.size();
+        urLs.add(longUrl);
+    	return String.valueOf(index);
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        int index=Integer.parseInt(shortUrl);
+    	return urLs.get(index);
+    }
+    
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
