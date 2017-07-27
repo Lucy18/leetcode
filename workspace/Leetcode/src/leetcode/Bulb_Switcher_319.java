c97d1a5dba63b44ef30ef260dce05eacb3b7b3dd
Lucy18 <916648359@qq.com>
2017-07-27 13:21:52 +0800

    319

---
+++ workspace/Leetcode/src/leetcode/Bulb_Switcher_319.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+public class Bulb_Switcher_319 {
+
+	public int bulbSwitch(int n) {
+		return (int)Math.sqrt(n);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Bulb_Switcher_319 b=new Bulb_Switcher_319();
+		System.out.println(b.bulbSwitch(10000000));
+	}
+
+}
