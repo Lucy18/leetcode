8e22c239deb0a2d890f6bc4988485e7510719119
Lucy18 <916648359@qq.com>
2017-08-21 14:25:14 +0800

    64

---
+++ workspace/Leetcode/src/leetcode/Minimum_Path_Sum_64.java
@@ -0,0 +1,39 @@
+package leetcode;
+
+public class Minimum_Path_Sum_64 {
+
+	public int minPathSum(int[][] grid) {
+		int m=grid.length;
+		int n=grid[0].length;
+		if(m==0||n==0){
+			return 0;
+		}
+		//dp[i][j]存储走到（i,j）这个点的最小sum
+		int[][] dp=new int[m][n];
+		dp[0][0]=grid[0][0];
+		for(int i=1;i<m;i++){
+			dp[i][0]=dp[i-1][0]+grid[i][0];
+		}
+		for(int i=1;i<n;i++){
+			dp[0][i]=dp[0][i-1]+grid[0][i];
+		}
+		for(int i=1;i<m;i++){
+			for(int j=1;j<n;j++){
+				dp[i][j]=Math.min(dp[i-1][j],dp[i][j-1])+grid[i][j];
+			}
+		}
+		return dp[m-1][n-1];
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Minimum_Path_Sum_64 m=new Minimum_Path_Sum_64();
+		int[][] grid=new int[][]{
+			{1,2,3},
+			{4,5,6},
+			{7,8,9}
+		};
+		System.out.println(m.minPathSum(grid));
+	}
+
+}
